203794518997c40443f21d0988b725b936af3b92
luiz fernando <107733670+luizfernandoLF@users.noreply.github.com>
2024-11-08 18:22:52 -0300

    Update tabela_hash.h

--- tabela_hash.h
+++ tabela_hash.h
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 typedef struct sensor {
     int pump_id;
@@ -12,7 +9,7 @@ typedef struct sensor {
     double rpm;
     double operational_hours;
     int maintenance_flag;
-    struct sensor* prox; 
+    struct sensor* prox;
 } Sensor;
 
 typedef struct elem_hash {
@@ -29,8 +26,10 @@ typedef struct {
 unsigned int funcao_hashing(t_hash* t, int chave);
 t_hash* criar_hash(int tamanho);
 Sensor* cria_sensor(int pump_id, int class_id, double temperature, double vibration, double pressure, double flow_rate, double rpm, double operational_hours, int maintenance_flag);
-
 void inserir_hash(t_hash* t, int chave, Sensor* sensor, FILE* saida);
 Sensor* buscar_hash(t_hash* t, int chave, FILE* saida);
 void remover_hash(t_hash* t, int chave, FILE* saida);
 void gerar_relatorio(t_hash* t, const char* comando, int chave, FILE* saida);
+void imprimirContadores();
+
+
