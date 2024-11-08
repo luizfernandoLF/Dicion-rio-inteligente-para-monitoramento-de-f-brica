a2752c903fefef0a8150c50f5c43a4d1c9b26722
luiz fernando <107733670+luizfernandoLF@users.noreply.github.com>
2024-11-07 23:33:22 -0300

    Update arvoreavl.h

--- arvoreavl.h
+++ arvoreavl.h
@@ -1,18 +1,27 @@
-typedef struct No{
+typedef struct Medicao {
+    double temperatura;
+    double vibracao;
+    double pressao;
+    struct Medicao* prox;
+} Medicao;
+
+typedef struct No {
     int chave;
-    struct No *esquerda;
-    struct No *direita;
+    Medicao* medicoes;
+    struct No* esquerda;
+    struct No* direita;
     int altura;
 } No;
 
 int altura(No *N);
 int max(int a, int b);
 No* novoNo(int chave);
+No* inserirNo(No* raiz, int chave, double temperatura, double vibracao, double pressao);
+void adicionarMedicao(No* sensor, double temperatura, double vibracao, double pressao);
+double maxMedicao(No* sensor);
+double minMedicao(No* sensor);
+double calcularMediaMedicoes(No* sensor);
 No *rotacaoDir(No *y);
 No *rotacaoEsq(No *x);
-int fatorBalanc(No *N);
-No* inserirNo(No* No, int chave);
-No *noDeValorMinimo(No *No);
 No* removerNo(No* raiz, int chave);
 No* buscarNo(No* raiz, int chave);
-void imprimirArvore(No *raiz);
